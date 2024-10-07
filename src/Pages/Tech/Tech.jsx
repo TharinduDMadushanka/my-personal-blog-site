51e9e1863a7ca5d32a77f2364754464c21dbd260
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-10-07 15:34:01 +0530

    update tech section background and

--- src/Pages/Tech/Tech.jsx
+++ src/Pages/Tech/Tech.jsx
@@ -6,6 +6,7 @@ import bg3 from '../../assets/pages/Tech/tech-bg4.jpg';
 import Navbar from '../../Components/Navbar/Navbar';
 import tech_data from '../../data/Tech/tech_data';
 import { Modal } from 'react-bootstrap';
+import bg_video from '../../assets/pages/Tech/bg.mp4'
 
 const Tech = () => {
   const [selectedPlace, setSelectedPlace] = useState(null);
@@ -24,33 +25,9 @@ const Tech = () => {
     <div className='tech'>
       <Navbar />
 
-      {/* Carousel Section */}
-      <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src={bg1} className="d-block w-100" alt="First Slide" />
-          </div>
-          <div className="carousel-item">
-            <img src={bg2} className="d-block w-100" alt="Second Slide" />
-          </div>
-          <div className="carousel-item">
-            <img src={bg3} className="d-block w-100" alt="Third Slide" />
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
+      <video className='background' autoPlay loop muted>
+        <source src={bg_video} type='video/mp4' />
+      </video>
 
       {/* Technology Blog Section */}
       <div className="tech-blog container mt-5">
